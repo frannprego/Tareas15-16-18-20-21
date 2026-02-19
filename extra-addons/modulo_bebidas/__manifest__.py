0aeb8d3440980bc01c10ac53f8509f6576a597da
fran <fpregodelgado@danielcastelao.org>
2026-02-19 16:08:46 +0100

    Tarea15

---
+++ extra-addons/modulo_bebidas/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Recomendador de Bebidas',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Asigna bebida según el sueño del alumno',
+    'depends': ['base'],
+    'data': [
+        'views/bebidas_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
